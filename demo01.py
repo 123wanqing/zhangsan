be8c10f9f6b2b72faed173d7b845851e87575af8
wanqing <1164098858@qq.com>
2020-04-03 18:33:06 +0800

    第一次向GitHub提交代码

---
+++ demo01.py
@@ -0,0 +1,26 @@
+'''
+print("你好") #字符串
+print(2333) #整数
+print(2.333) #小数
+print(True) #布尔值
+print(()) #元组
+print([]) #数组
+print({}) #字典
+啦啦啦啦
+哈哈哈哈
+print("哈哈哈"*100)
+name = "张三"
+print(name)
+'''
+# a = input("请输入：")
+# print("input获取的内容：",a)
+# a = int("2333")
+# print(type(a))
+# a = int(input("请输入："))
+# b = float(input("请输入："))
+# print("input获取的内容",a+b)
+a = "nibuaiwole"
+b =len(a)
+c = "2333"
+d = len(c)
+print(b+d)
